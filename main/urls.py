fca0d771cbd68ecf630f7658a772915faebdd192
Edan Ben-Atar <benatare@gmail.com>
2021-01-25 09:19:22 -0500

    project application and portfolio page

--- main/urls.py
+++ main/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', include('blog.urls')),
     path('who-we-are/', views.who_we_are, name='who-we-are'),
     path('solutions/', views.solutions, name='solutions'),
+    path('portfolio/', views.portfolio, name='portfolio'),
     path('get-in-touch/', views.get_in_touch, name='get-in-touch'),
+    path('new-project-application/', views.new_project_application, name='new-project-aplication'),
 ]
--- main/views.py
+++ main/views.py
@@ -15,6 +15,8 @@ def home(request):
 def who_we_are(request):
     return render(request, 'who-we-are.html', {'title': 'Who We Are'})
 
+def portfolio(request):
+    return render(request, 'portfolio.html', {'title': 'Portfolio'})
 
 def solutions(request):
     return render(request, 'solutions.html', {'title': 'Solutions'})
@@ -52,3 +54,37 @@ def get_in_touch(request):
         form = getInTouchForm()
 
     return render(request, 'get-in-touch.html', {'form': form, 'title': 'Get In Touch'})
+
+
+def new_project_application(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = newProjectApplication(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            subject = "New Project Application"
+            sender_email = 'support@WebLime.com'
+            recipients = ['edan@weblime.com']
+            body = {
+			'fullname': form.cleaned_data['fullname'],
+			'email': form.cleaned_data['email'],
+			'phone': form.cleaned_data['phone'],
+			'message':form.cleaned_data['message']
+			}
+
+            message = "\n".join(body.values())
+
+            #send send_mail
+            send_mail(subject, message, sender_email, recipients)
+
+            # redirect to a new URL:
+            messages.success(request, f'Your form has been submited!')
+            return redirect('home')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = getInTouchForm()
+
+    return render(request, 'new-project-application.html', {'form': form, 'title': 'New Project Application'})
